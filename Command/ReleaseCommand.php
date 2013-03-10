817e49a10275ad8e80ac3c588c7fd7648094fc12
Erik Trapman <veggatron@gmail.com>
2013-03-10 13:23:46 +0100

    fully working 0.1

---
+++ Command/ReleaseCommand.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace ErikTrapman\Bundle\ReleaseCommandBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\ProcessBuilder;
+
+class ReleaseCommand extends ContainerAwareCommand
+{
+
+    /**
+     * 
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('eriktrapman:release')
+            ->addArgument('target-dir', \Symfony\Component\Console\Input\InputArgument::REQUIRED)
+            ->setDescription("Prepares a release of a standard Symfony2-project by copying all files needed to a given path")
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $start = microtime(1); // TODO use StopWatch
+        $targetDir = $input->getArgument('target-dir');
+        $kernel = $this->getContainer()->get('kernel');
+        $rootDir = $kernel->getRootDir();
+
+        $f = new ExecutableFinder();
+        $git = $f->find('git');
+        if (!$git) {
+            throw new \RuntimeException("This command cannot run without GIT.");
+        }
+        $tarname = uniqid().'.tar';
+        $pb = ProcessBuilder::create(array($git, 'archive', '-o', $tarname, 'HEAD'));
+        $p = $pb->getProcess();
+        $p->run(function ($type, $buffer) {
+                if ('err' === $type) {
+                    $output->write('ERR > '.$buffer);
+                } else {
+                    $output->write('OUT > '.$buffer);
+                }
+            });
+        if ($errorOutput = $p->getErrorOutput()) {
+            $output->writeln(nl2br($errorOuput, false));
+        }
+        $pb = ProcessBuilder::create(array('tar', '-xf', $tarname, '-C', $targetDir));
+        $p = $pb->getProcess();
+        $p->run();
+
+        $finder = new \Symfony\Component\Finder\Finder();
+        $finder->in($rootDir.'/../')->ignoreDotFiles(true)->ignoreVCS(true);
+        $filesystem = new \Symfony\Component\Filesystem\Filesystem();
+        $filesystem->mirror($rootDir.'/../vendor/', $targetDir.'/vendor/', $finder);
+
+        $this->injectApplicationVitals($rootDir, $targetDir);
+        $filesystem->remove($rootDir.'/../'.$tarname);
+        $d = microtime(1) - $start;
+        $output->writeln('Released in '.$d.' microseconds');
+        return;
+    }
+
+    private function injectApplicationVitals($rootDir, $targetDir)
+    {
+        $ignoredFiles = file($rootDir.'/../.gitignore');
+        $fileSystem = new \Symfony\Component\Filesystem\Filesystem();
+        foreach ($ignoredFiles as $ignoreFile) {
+            $ignoreFile = trim($ignoreFile);
+            if (0 == strlen($ignoreFile) || false !== strpos($ignoreFile, '#')) {
+                continue;
+            }
+            if (false !== strpos($ignoreFile, 'app/bootstrap')) {
+                $fileSystem->copy($rootDir.'/bootstrap.php.cache', $targetDir.'/app/bootstrap.php.cache');
+            }
+            if (false !== strpos($ignoreFile, 'app/config/parameters')) {
+                if ($fileSystem->exists($rootDir.'/config/parameters.yml')) {
+                    $fileSystem->copy($rootDir.'/config/parameters.yml', $targetDir.'/app/config/parameters.yml');
+                }
+                if ($fileSystem->exists($rootDir.'/config/parameters.ini')) {
+                    $fileSystem->copy($rootDir.'/config/parameters.ini', $targetDir.'/app/config/parameters.ini');
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 ReleaseCommandBundle
 ====================
+
+Prepares a release of a standard Symfony2-project by copying all files needed to a given path.
+Useful when you have to FTP all of your files individually to a shared-hosting environment.
+
+Please note, this script does not support the use of git-submodules, simply because it uses git-archive, and this does not work with git-submodules.
+
+TODO
+===================
+* Support use of git-submodules?
+* Windows-support needed? (not even tested yet)
+* 
